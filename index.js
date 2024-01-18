6d4a1c70735a03d5a618826e6ed015f26f667630
samharris0509 <samhancock2023@gmail.com>
2024-01-18 14:23:05 -0600

    message

---
+++ index.js
@@ -0,0 +1,7 @@
+//sam harris 1/18/2024
+let theNumber = 8;
+theNumber = theNumber * 2;
+
+let userValue = prompt("What number do you like?");
+console.log("theNumber:", theNumber);
+console.log("userValue:", userValue);
