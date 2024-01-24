858cebdd17679bfd4f4bc574a555beeed0429040
samharris0509 <samhancock2023@gmail.com>
2024-01-24 12:06:31 -0600

    java

---
+++ arrays.js
@@ -0,0 +1,22 @@
+
+let Array = [10, 20, 30, 40];
+
+
+let addOne = function (param) {
+  param += 1;
+  console.log(param);
+};
+
+
+let checkAndCall = function (func, number) {
+  if (number % 2 === 0) {
+    func(number);
+  } else {
+    console.log("the number is odd");
+  }
+};
+
+
+for (let index in Array) {
+  checkAndCall(addOne, Array[index]);
+}
